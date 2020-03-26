1f64f7d993dc8a013f13e8408148a912229e263c
paulus <paulus@DESKTOP-UBIC4EC>
2020-03-26 20:28:30 +0900

    2020.03.26 pm08:28 김경재
    메인페이지
    인기, 신간
    도서 이미지 슬라이드완성

--- Bookflix/src/main/java/com/green/main/model/MainDAO.java
+++ Bookflix/src/main/java/com/green/main/model/MainDAO.java
@@ -1,5 +1,14 @@
 package com.green.main.model;
 
+import java.util.List;
+
+import com.green.main.domain.MainDTO;
+
 public interface MainDAO {
+  
+//  인기도서 목록
+  public List<MainDTO> favorite() throws Exception;
+//  신간도서 목록
+  public List<MainDTO> newbook() throws Exception;
 
 }
--- Bookflix/src/main/java/com/green/main/model/MainDAOImpl.java
+++ Bookflix/src/main/java/com/green/main/model/MainDAOImpl.java
@@ -1,5 +1,34 @@
 package com.green.main.model;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.green.main.domain.MainDTO;
+
+@Repository
 public class MainDAOImpl implements MainDAO {
+
+  @Inject
+  private SqlSession sql;
+
+  private static String namespace = "com.green.mapper.mainMapper";
+
+  // 인기도서 목록
+  @Override
+  public List<MainDTO> favorite() throws Exception {
+
+    return sql.selectList(namespace + ".favorite");
+  }
+  
+  // 신간도서 목록
+  @Override
+  public List<MainDTO> newbook() throws Exception {
+   
+    return sql.selectList(namespace + ".newbook");
+  }
 
 }
