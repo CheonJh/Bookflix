719557e95bd43acca3072d9755a42f6a560553c4
SungHoon <SungHoon@DESKTOP-S3DOCBR>
2020-03-30 20:44:32 +0900

    김성훈 2020 03/30
    PM08:43
    
    공지사항/FAQ
    카테고리별 보기 정리

--- Bookflix/src/main/java/com/green/info/model/InfoDAOImpl.java
+++ Bookflix/src/main/java/com/green/info/model/InfoDAOImpl.java
@@ -9,6 +9,7 @@ import javax.xml.stream.events.Namespace;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.green.info.domain.InfoFaqDTO;
 import com.green.info.domain.InfoNoticeDTO;
 
 @Repository
@@ -20,11 +21,9 @@ public class InfoDAOImpl implements InfoDAO {
   private static String namespace = "com.green.mapper.infoMapper";
 
   @Override
-  public List<InfoNoticeDTO> list() throws Exception {
-
-    return sql.selectList(namespace + ".list");
+  public List<InfoNoticeDTO> notiList() throws Exception {
+    return sql.selectList(namespace + ".notiList");
   }
-
   
   public InfoNoticeDTO notice(int notice_num) throws Exception {
     // TODO Auto-generated method stub
@@ -32,22 +31,124 @@ public class InfoDAOImpl implements InfoDAO {
   }
   
   @Override
-  public int count() throws Exception {
+  public int notiCount() throws Exception {
     // TODO Auto-generated method stub
-    return sql.selectOne(namespace + ".count");
+    return sql.selectOne(namespace + ".notiCount");
   }
   
   @Override
   public List<InfoNoticeDTO> noticePage(int displayPost, int postNum) throws Exception {
 
-    HashMap data = new HashMap();
+    HashMap<String, Integer> data = new HashMap<String, Integer>();
     
     data.put("displayPost", displayPost);
     data.put("postNum", postNum);
     
+    
     return sql.selectList(namespace + ".noticePage", data);
   }
   
+  @Override
+  public List<InfoNoticeDTO> noticePage2(int displayPost, int postNum) throws Exception {
+
+    HashMap<String, Integer> data = new HashMap<String, Integer>();
+    
+    data.put("displayPost", displayPost);
+    data.put("postNum", postNum);
+    
+    
+    return sql.selectList(namespace + ".noticePage2", data);
+  }
   
+  @Override
+  public List<InfoNoticeDTO> noticePage3(int displayPost, int postNum) throws Exception {
+
+    HashMap<String, Integer> data = new HashMap<String, Integer>();
+    
+    data.put("displayPost", displayPost);
+    data.put("postNum", postNum);
+    
+    
+    return sql.selectList(namespace + ".noticePage3", data);
+  }
+  
+  @Override
+  public List<InfoNoticeDTO> noticePage4(int displayPost, int postNum) throws Exception {
+
+    HashMap<String, Integer> data = new HashMap<String, Integer>();
+    
+    data.put("displayPost", displayPost);
+    data.put("postNum", postNum);
+    
+    
+    return sql.selectList(namespace + ".noticePage4", data);
+  }
+
+  @Override
+  public void viewCnt(int notice_num) throws Exception {
+       sql.update(namespace + ".viewCnt", notice_num);
+  }
+
+
+  @Override
+  public List<InfoFaqDTO> FaqList() throws Exception {
+    
+    return sql.selectList(namespace + ".FaqList");
+  }
+
+
+  @Override
+  public List<InfoFaqDTO> FaqPage(int displayPost, int postNum) throws Exception {
+    HashMap<String, Integer> data = new HashMap<String, Integer>();
+    
+    data.put("displayPost", displayPost);
+    data.put("postNum", postNum);
+    
+    return sql.selectList(namespace + ".FaqPage", data);
+  }
+  
+  @Override
+  public List<InfoFaqDTO> FaqPage2(int displayPost, int postNum) throws Exception {
+    HashMap<String, Integer> data = new HashMap<String, Integer>();
+    
+    data.put("displayPost", displayPost);
+    data.put("postNum", postNum);
+    
+    return sql.selectList(namespace + ".FaqPage2", data);
+  }
+  
+  @Override
+  public List<InfoFaqDTO> FaqPage3(int displayPost, int postNum) throws Exception {
+    HashMap<String, Integer> data = new HashMap<String, Integer>();
+    
+    data.put("displayPost", displayPost);
+    data.put("postNum", postNum);
+    
+    return sql.selectList(namespace + ".FaqPage3", data);
+  }
+  
+  @Override
+  public List<InfoFaqDTO> FaqPage4(int displayPost, int postNum) throws Exception {
+    HashMap<String, Integer> data = new HashMap<String, Integer>();
+    
+    data.put("displayPost", displayPost);
+    data.put("postNum", postNum);
+    
+    return sql.selectList(namespace + ".FaqPage4", data);
+  }
+
+
+  @Override
+  public InfoFaqDTO FAQ(int faq_num) throws Exception {
+    
+    return sql.selectOne(namespace + ".FAQ", faq_num);
+  }
+
+
+  @Override
+  public int faqCount() throws Exception {
+    
+    return sql.selectOne(namespace + ".faqCount");
+  }
   
 }
--- Bookflix/src/main/webapp/WEB-INF/views/info/FAQList2.jsp
+++ Bookflix/src/main/webapp/WEB-INF/views/info/FAQList2.jsp
@@ -10,13 +10,31 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="author" content="김성훈" />
-<meta name="description" content="공지사항 목록 페이지" />
+<meta name="description" content="FAQ 목록 페이지" />
 
 <title>TestPageForm</title>
 
 <style type="text/css">
+#notignb {
+  width: 100%;
+  height: 200px;
+  margin: 0 auto;
+}
+
+#notignb ul {
+ padding-top: 15px;
+ 
+}
+
+#notignb ul li {
+  display: inline;
+  font: bold 25px;
+}
+
+
 li {
   list-style: none;
+  
 }
 
 .active {
@@ -36,14 +54,13 @@ li {
   max-width: 600px;
 }
 
-.tb-nb {
+.tb-nb{
   width: 100px;
 }
 
 .tb-ct {
   width: 100px;
 }
-
 .tb-vc {
   width: 100px;
 }
@@ -62,17 +79,18 @@ a, a:hover {
   text-decoration: none;
 }
 </style>
-
 </head>
 <body>
+ 
   <div id="notignb">
     <ul class="clearfix">
-      <li><a href="/info/noticeList?num=1">전체보기</a></li>
-      <li><a href="/info/noticeList2?num=1">공지사항</a></li>
-      <li><a href="/info/noticeList3?num=1">업데이트</a></li>
-      <li><a href="/info/noticeList4?num=1">이벤트</a></li>
+      <li><a href="/info/FAQList?num=1">전체보기</a></li>
+      <li><a href="/info/FAQList2?num=1">이용방법</a></li>
+      <li><a href="/info/FAQList3?num=1">구독</a></li>
+      <li><a href="/info/FAQList4?num=1">자주하는 질문</a></li>
     </ul>
   </div>
+
   <div class="container">
     <table class="table table-bordered tbFAQ">
       <thead>
@@ -80,41 +98,38 @@ a, a:hover {
           <th class="tb-nb">번호</th>
           <th class="tb-ct">항목</th>
           <th class="tb-cl">내용</th>
-          <th class="tb-vc">조회수</th>
-          <th class="tb-ap">등록일</th>
         </tr>
       </thead>
-      <c:forEach items="${notiList}" var="notiList">
+      <c:forEach items="${FaqList}" var="FaqList">
         <tr>
-          <td>${notiList.notice_num}</td>
-          <td>${notiList.notice_category}</td>
+          <td>${FaqList.faq_num}</td>
+          <td>${FaqList.faq_category}</td>
           <td><a
-            href="/info/viewCnt?notice_num=${notiList.notice_num}">${notiList.notice_title}</a></td>
-          <td><c:out value="${notiList.notice_viewcnt}" /></td>
-          <td><fmt:formatDate value="${notiList.notice_date}"
-              pattern="yyyy-MM-dd"></fmt:formatDate></td>
+            href="/info/FAQ?faq_num=${FaqList.faq_num}">${FaqList.faq_title}</a></td>
         </tr>
       </c:forEach>
     </table>
-  </div>
-
+    </div>
   <div>
-    <c:if test="${prev}">
-      <span>[<a href="/info/noticeList?num=${startPageNum -1}">이전</a>]
-      </span>
-    </c:if>
-    <c:forEach begin="${startPageNum}" end="${endPageNum}" var="num">
-      <span> <c:if test="${select != num}">
-          <a href="/info/noticeList?num=${num}">${num}</a>
-        </c:if> <c:if test="${select == num}">
+  <c:if test="${prev}">
+    <span>[<a href="/info/FAQList2?num=${startPageNum -1}">이전</a>]</span>
+  </c:if>
+  
+  <c:forEach begin="${startPageNum}" end="${endPageNum}" var="num">
+    <span>
+      <c:if test="${select != num}">
+        <a href="/info/FAQList2?num=${num}">${num}</a>
+       </c:if>
+       
+       <c:if test="${select == num}">
           <b>${num}</b>
-        </c:if>
-      </span>
-    </c:forEach>
-    <c:if test="${next}">
-      <span>[<a href="/info/noticeList?num=${endPageNum +1}">다음</a>]
-      </span>
-    </c:if>
+       </c:if>
+    </span>
+  </c:forEach>
+  
+  <c:if test="${next}">
+    <span>[<a href="/info/FAQList2?num=${endPageNum +1}">다음</a>]</span>
+  </c:if>
   </div>
 </body>
 </html>
